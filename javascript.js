42469904483b82df875fa088d3df3c6e5f52a4b0
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2025-03-20 18:14:47 +0100

    Klar med varukorg js

--- javascript.js
+++ javascript.js
@@ -5,14 +5,56 @@ var btn = document.getElementById("Varukorg-btn");
 
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
     Varukorg.style.display = "block";
 }
 
-span.onclick = function() {
+span.onclick = function () {
     Varukorg.style.display = "none";
 }
 
-function Läggatill(Title) {
-    let Eld = document
+const ul = document.querySelector("ul");
+const MAX=5;
+let count=0;
+
+function Läggatill(title, photo, cost, remove) {
+
+    Varukorg.style,display = "block";
+
+
+    if (count<MAX) {
+        let li = document.createElement("li");
+        li.textContent= title + photo + cost + remove;
+        ul.appendChild(li);
+        count++;
+    }
+
+    let fält =[];
+    let json = window.localStorage.getItem("min_nyckel");
+    if (json) {
+        fält = JSON.parse(json);
+    }
+
+    let obj = {
+        title: title,
+        photo: photo,
+        cost: cost,
+        remove: remove
+    }
+    fält.push(obj);
+    json = JSON.stringify(fält);
+    window.localStorage.setItem("min_nyckel", json)
+}
+
+function laddat_sidan() {
+    let fält = [];
+    let json = window.localStorage.getItem("min_nyckel");
+    if (json) {
+        fält = JSON.parse(json);
+    }
+
+    for (let i=0; i<array.length; i++) {
+        
+        
+    }
 }
